c9e999527a0efe6abd633e57081c15c0e155dfc5
WesMatthews <wmatthews@infotech.com>
2015-04-17 19:27:45 -0400

    employee printing

--- PPSoft_SkedgeIT/PPSoft_SkedgeIT/EmployeeLandingPage.xaml.cs
+++ PPSoft_SkedgeIT/PPSoft_SkedgeIT/EmployeeLandingPage.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using PPSoft_SkedgeITViewModels;
+using System.Printing;
+using System.IO;
+using System.Windows.Markup;
 
 namespace PPSoft_SkedgeIT
 {
@@ -20,15 +23,50 @@ namespace PPSoft_SkedgeIT
     /// </summary>
     public partial class EmployeeLandingPage : Window
     {
+        List<ShiftViewModel> shifts;
         public EmployeeLandingPage(EmployeeViewModel emp)
         {
             InitializeComponent();
             //bind data grid to list
             ShiftViewModel shiftObject = new ShiftViewModel();
-            List<ShiftViewModel> shifts;
+
             shifts = shiftObject.getShifts(emp.EmployeeID);
             shiftGrid.ItemsSource = shifts;
 
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            StreamWriter file = new StreamWriter("temp.txt");
+            file.WriteLine(shifts);
+            file.Close();
+            PrintDialog prtDiag = new PrintDialog();
+            if (prtDiag.ShowDialog() != true) return;
+
+            // create a document
+            FixedDocument document = new FixedDocument();
+            document.DocumentPaginator.PageSize = new Size(prtDiag.PrintableAreaWidth, prtDiag.PrintableAreaHeight);
+            // create a page
+            FixedPage page1 = new FixedPage();
+            page1.Width = document.DocumentPaginator.PageSize.Width;
+            page1.Height = document.DocumentPaginator.PageSize.Height;
+            // add some text to the page
+            TextBlock page1Text = new TextBlock();
+            foreach (ShiftViewModel s in shifts)
+            {
+                page1Text.Text += s.ToString() + "\n";
+            }
+            //page1Text.Text = shiftGrid.Items.ToString();
+            page1Text.FontSize = 15; // 30pt text
+            page1Text.Margin = new Thickness(96); // 1 inch margin
+            page1.Children.Add(page1Text);
+            // add the page to the document
+            PageContent page1Content = new PageContent();
+            ((IAddChild)page1Content).AddChild(page1);
+            document.Pages.Add(page1Content);
+            // and print
+            prtDiag.PrintDocument(document.DocumentPaginator, "My first document");
         }
     }
 }
